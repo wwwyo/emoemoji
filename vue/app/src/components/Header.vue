8fb4230548a4c3ec017bfc640cd4075fe416a53f
yuito118 <72675978+yuito118@users.noreply.github.com>
2022-03-21 11:00:25 +0900

    add header

---
+++ vue/app/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app-bar color="primary" density="compact">
+    <v-app-bar-title>
+      <div class="d-flex flex-row">
+        &#x1F618;&#x1F928;&#x1F60E;
+        <v-img :src="logo" width="90" height="35"></v-img>
+        &#x1F618;&#x1F928;&#x1F60E;
+      </div>
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  setup() {
+    const logo = require('@/assets/img/logo_w.png');
+    return {
+      logo,
+    };
+  },
+};
+</script>
