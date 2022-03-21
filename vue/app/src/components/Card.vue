cba1c540b79379a3fcdf830c5661b7e358e19c6d
yuito118 <72675978+yuito118@users.noreply.github.com>
2022-03-21 23:30:40 +0900

    add: card

---
+++ vue/app/src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-col cols="3">
+    <v-card class="pa-2 d-flex" align="center" outlined tile>
+      <v-img :src="src" height="80px" cover></v-img>
+      <v-card-title>
+        <v-row align="center" class="mx-0"> aaaaaaaa</v-row>
+      </v-card-title>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {
+  props: {
+    src: String,
+  },
+};
+</script>
