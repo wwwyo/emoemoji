239570615851a4e948a58c129bc36ff411238f91
yuito118 <72675978+yuito118@users.noreply.github.com>
2022-03-21 23:32:41 +0900

    update:headerの幅

--- vue/app/src/components/Header.vue
+++ vue/app/src/components/Header.vue
@@ -1,12 +1,14 @@
 <template>
   <v-app-bar color="primary" density="compact">
-    <v-app-bar-title>
-      <div class="d-flex flex-row">
-        &#x1F618;&#x1F928;&#x1F60E;
-        <v-img :src="logo" width="90" height="35"></v-img>
-        &#x1F618;&#x1F928;&#x1F60E;
-      </div>
-    </v-app-bar-title>
+    <v-container>
+      <v-app-bar-title>
+        <div class="d-flex flex-row">
+          &#x1F618;&#x1F928;&#x1F60E;
+          <v-img :src="logo" width="90" height="35"></v-img>
+          &#x1F618;&#x1F928;&#x1F60E;
+        </div>
+      </v-app-bar-title>
+    </v-container>
   </v-app-bar>
 </template>
 
