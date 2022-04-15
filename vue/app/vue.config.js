dbade4860fc7a104835ca4d7d2777f9b6d80f06a
yuito118 <72675978+yuito118@users.noreply.github.com>
2022-04-15 17:42:50 +0900

    update:deploy

--- vue/app/vue.config.js
+++ vue/app/vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
+  outputDir: 'dist',
+  assetsDir: './',
+  publicPath: './',
   transpileDependencies: true,
   pluginOptions: {
     vuetify: {
